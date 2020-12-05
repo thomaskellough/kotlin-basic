a78fe5fe3b460ff486ff973e5ec2023d5bf5644c
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-12-05 16:30:26 -0700

    added diceroll for button click and dice class

---
+++ app/src/main/java/com/kellough/diceroller/Dice.kt
@@ -0,0 +1,7 @@
+package com.kellough.diceroller
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
--- app/src/main/java/com/kellough/diceroller/MainActivity.kt
+++ app/src/main/java/com/kellough/diceroller/MainActivity.kt
@@ -3,17 +3,25 @@ package com.kellough.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.rollButton)
-        rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-        }
+
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val rollTextView: TextView = findViewById(R.id.rollTextView)
+        val d6 = Dice(numSides = 6)
+
+        rollTextView.text = d6.roll().toString()
     }
 }

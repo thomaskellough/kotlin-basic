ffb1021ad5b8eb09a6dbba7690341db1da48a19e
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-12-05 16:48:16 -0700

    added images for dice

--- app/src/main/java/com/kellough/diceroller/MainActivity.kt
+++ app/src/main/java/com/kellough/diceroller/MainActivity.kt
@@ -3,9 +3,7 @@ package com.kellough.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,12 +14,23 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.rollButton)
 
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
     private fun rollDice() {
-        val rollTextView: TextView = findViewById(R.id.rollTextView)
         val d6 = Dice(numSides = 6)
+        val imageView: ImageView = findViewById(R.id.diceImageView)
 
-        rollTextView.text = d6.roll().toString()
+        val randomRoll = d6.roll()
+        when (randomRoll) {
+            1 -> imageView.setImageResource(R.drawable.dice_1)
+            2 -> imageView.setImageResource(R.drawable.dice_2)
+            3 -> imageView.setImageResource(R.drawable.dice_3)
+            4 -> imageView.setImageResource(R.drawable.dice_4)
+            5 -> imageView.setImageResource(R.drawable.dice_5)
+            6 -> imageView.setImageResource(R.drawable.dice_6)
+        }
+        imageView.contentDescription = randomRoll.toString()
     }
 }

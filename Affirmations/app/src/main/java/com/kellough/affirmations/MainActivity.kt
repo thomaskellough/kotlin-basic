2eb4607bdcb350f0922a78a20fa8f12868be8ab6
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-12-06 11:49:43 -0700

    added databinding and recycler view now shows affirmations

--- Affirmations/app/src/main/java/com/kellough/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/kellough/affirmations/MainActivity.kt
@@ -2,11 +2,20 @@ package com.kellough.affirmations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.kellough.affirmations.adapter.ItemAdapter
+import com.kellough.affirmations.data.DataSource
+import com.kellough.affirmations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val dataset = DataSource().loadAffirmations()
+        binding.affirmationsRecyclerView.adapter = ItemAdapter(this, dataset)
+        binding.affirmationsRecyclerView.setHasFixedSize(true) // only used for performance - do not use if you you need different sizes
     }
 }

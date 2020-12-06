87a057701a191af8a302387cbe276f005e784d59
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-12-05 22:45:13 -0700

    refractored to make cleaner

--- TipCalculator/app/src/main/java/com/kellough/tiptime/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/kellough/tiptime/MainActivity.kt
@@ -8,7 +8,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +23,11 @@ class MainActivity : AppCompatActivity() {
         if(initialCost == null) {
             val toast = Toast.makeText(this,"Please enter an initial amount.", Toast.LENGTH_SHORT)
             toast.show()
+            binding.tipResult.text = ""
             return
         }
 
-        val tipIndex = binding.tipOptions.checkedRadioButtonId
-
-        val tipPercentage = when (tipIndex) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -38,7 +37,11 @@ class MainActivity : AppCompatActivity() {
         if(binding.roundUpSwitch.isChecked)
             tipAmount = kotlin.math.ceil(initialCost * tipPercentage)
 
-        var formattedTip = NumberFormat.getCurrencyInstance().format(tipAmount)
+        displayTip(tipAmount)
+    }
+
+    private fun displayTip(tip: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }

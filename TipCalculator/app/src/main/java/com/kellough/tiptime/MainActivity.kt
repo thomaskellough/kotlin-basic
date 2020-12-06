e72f641d134b5f28afd26e6b64b56373413ffef2
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-12-06 00:35:09 -0700

    added function to dismiss keyboard when enter is pressed

--- TipCalculator/app/src/main/java/com/kellough/tiptime/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/kellough/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.kellough.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kellough.tiptime.databinding.ActivityMainBinding
@@ -16,12 +20,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+        // Attach listener to dismiss keyboard when enter is pressed
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calculateTip() {
         val initialCost = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
-        if(initialCost == null) {
-            val toast = Toast.makeText(this,"Please enter an initial amount.", Toast.LENGTH_SHORT)
+        if (initialCost == null) {
+            val toast = Toast.makeText(this, "Please enter an initial amount.", Toast.LENGTH_SHORT)
             toast.show()
             binding.tipResult.text = ""
             return
@@ -34,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         var tipAmount = initialCost * tipPercentage
-        if(binding.roundUpSwitch.isChecked)
+        if (binding.roundUpSwitch.isChecked)
             tipAmount = kotlin.math.ceil(initialCost * tipPercentage)
 
         displayTip(tipAmount)
@@ -43,5 +51,17 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    // Hides keyboard when enter is pressed
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }

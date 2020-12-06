7b266845675f631b40d70013b5cdbd48d7d5c257
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-12-06 11:13:03 -0700

    added recycler view

--- Affirmations/app/src/main/java/com/kellough/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/kellough/affirmations/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.kellough.affirmations
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.kellough.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.textView)
-        textView.text = DataSource().loadAffirmations().size.toString()
     }
 }

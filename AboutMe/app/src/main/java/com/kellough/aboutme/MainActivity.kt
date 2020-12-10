4400bec5177ffd9eb6aa633a5a6131bb23558c3a
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-12-09 21:02:48 -0700

    added ability to change nickname after submitting

--- AboutMe/app/src/main/java/com/kellough/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/kellough/aboutme/MainActivity.kt
@@ -17,6 +17,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
+
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
+        }
     }
 
     private fun addNickname(view: View) {
@@ -31,5 +35,14 @@ class MainActivity : AppCompatActivity() {
         // Hide keyboard
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
     }
 }

4289f02038c870001138cf40671f629114311ce0
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-12-09 22:45:42 -0700

    added onClickListeners for buttons

--- ColorMyViews/app/src/main/java/com/kellough/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/kellough/colormyviews/MainActivity.kt
@@ -23,14 +23,17 @@ class MainActivity : AppCompatActivity() {
             binding.boxThree -> view.setBackgroundResource(R.drawable.image3)
             binding.boxFour -> view.setBackgroundResource(R.drawable.image4)
             binding.boxFive -> view.setBackgroundResource(R.drawable.image5)
+            binding.redButton -> view.setBackgroundResource(R.color.my_red)
+            binding.greenButton -> view.setBackgroundResource(R.color.my_green)
+            binding.yellowButton -> view.setBackgroundResource(R.color.my_yellow)
             else -> view.setBackgroundResource(R.drawable.image6)
         }
     }
 
     private fun setListeners() {
         val clickableViews: List<View> = listOf(binding.boxOne, binding.boxTwo,
-                                                binding.boxThree, binding.boxFour,
-                                                binding.boxFive, binding.root)
+            binding.boxThree, binding.boxFour, binding.boxFive, binding.root,
+            binding.redButton, binding.greenButton, binding.yellowButton)
 
         for (view in clickableViews) {
             view.setOnClickListener { makeColored(it) }

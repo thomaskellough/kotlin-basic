98cfacf9de03721da8d43eea7edeaacccd1bb9e4
thomas.kellough <thomas.kellough@brandtinfo.com>
2020-12-10 23:09:33 -0700

    added hamburger icon to drawer layout

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -13,23 +13,25 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+                R.layout.fragment_title,container,false)
 
-        binding.playButton.setOnClickListener{ view: View ->
+        binding.playButton.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
-
         setHasOptionsMenu(true)
         return binding.root
     }
 
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.options_menu, menu)
+        inflater.inflate(R.menu.options_menu,menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) ||
-                onOptionsItemSelected(item)
+        return NavigationUI.onNavDestinationSelected(item,view!!.findNavController())
+                ||super.onOptionsItemSelected(item)
     }
+
 }
